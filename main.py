bc5f1bdbbfcd83df81fcc9739141580b9f10138a
leonorf20 <162609318+leonorf20@users.noreply.github.com>
2025-05-18 10:21:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import read_data
+import feature_extraction
 import signal_processing
 import data_processing
 import umap_reduction
+import dbscan
 import pandas as pd
 
 data_dict = read_data.readData('Continuous\Continuous', 15)
 processed_dict = signal_processing.FilterSignals(data_dict, fs=1000) 
-#signal_processing.plot_signals(data_dict, processed_dict, key='continuous1', fs=1000, duration=10)
-features = read_data.featureVect(processed_dict)
+features = feature_extraction.featureVect(processed_dict)
 df_features = pd.DataFrame(features)
 Data = data_processing.Standardization(df_features)
 embedding = umap_reduction.dimensionality_reduce(Data)
-labels = umap_reduction.dbscan_clustering(embedding, eps=0.3, min_samples=10)
-filtered_embedding, filtered_labels = umap_reduction.remove_outliers(embedding, labels)
-umap_reduction.plot_embedding(filtered_embedding, labels=filtered_labels)
+labels = dbscan.dbscan_find_3_clusters(embedding)
+filtered_embedding, filtered_labels = dbscan.remove_outliers(embedding, labels)
+dbscan.plot_embedding(filtered_embedding, labels=filtered_labels)
 
 k_means = kmeans.Kmeans_method(embedding, 3)
 k_means_embedding, k_means_labels = umap_reduction.remove_outliers(embedding, k_means)
