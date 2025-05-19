c16883b3ff19446c27253355014e149d686db158
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-05-19 18:40:37 +0100

    Update supervised.py

--- supervised.py
+++ supervised.py
@@ -1,7 +1,15 @@
 import data_splits
-import RF
 import numpy as np
 import pandas as pd
+import SVM
 
-# Load and split data
 X_train, X_val, X_test, y_train, y_val, y_test = data_splits.load_and_split_data()
+
+param_grid = {
+   'kernel': ['linear', 'rbf'],
+   'C': [0.1, 1, 10],
+   'gamma': ['scale', 'auto'],
+}
+
+best_model = SVM.gridsearch(X_train, X_val, y_train, y_val, param_grid)
+SVM.svm_test(best_model, X_test, y_test)
