7cbb752e478a7847493950e51ec9d9640825a6eb
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-05-19 16:33:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ k_means_embedding, k_means_labels = umap_reduction.remove_outliers(embedding, k_
 mask = np.isin(k_means, k_means_labels)
 filtered_data = Data[mask]
 filtered_labels = k_means_labels
-Labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=8)
-Labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=9)
-Labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=13)
+labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=8)
+labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=9)
+labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=13)
