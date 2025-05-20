acc582033ff9e653bfdc36b5254690a470b4db05
leonorf20 <162609318+leonorf20@users.noreply.github.com>
2025-05-20 10:06:10 +0100

    Update unsupervised.py

--- unsupervised.py
+++ unsupervised.py
@@ -6,12 +6,15 @@ import umap_reduction
 import dbscan
 import pandas as pd
 import kmeans
-import labeling
+import labelling
 import silhouette
 import os
 import numpy as np
 
-data_dict = read_data.readData('Continuous', 15)
+# Change according to dataset location
+data_dict = read_data.readData('Continuous\Continuous', 15)
+
+os.makedirs('supervised_output', exist_ok=True)
 
 processed_dict = signal_processing.FilterSignals(data_dict, fs=1000) 
 features = feature_extraction.featureVect(processed_dict)
@@ -21,27 +24,25 @@ embedding = umap_reduction.dimensionality_reduce(Data)
 
 dbscan_labels = dbscan.dbscan_find_3_clusters(embedding)
 filtered_embedding, filtered_labels = dbscan.remove_outliers(embedding, dbscan_labels)
-dbscan.plot_embedding(filtered_embedding, labels=filtered_labels)
+umap_reduction.plot_embedding(filtered_embedding, labels=filtered_labels, save_path='supervised_output/dbscan.png')
 
 kmeans_labels = kmeans.Kmeans_method(filtered_embedding, 3)
-dbscan.plot_embedding(filtered_embedding, labels=kmeans_labels)
+umap_reduction.plot_embedding(filtered_embedding, labels=kmeans_labels, save_path='supervised_output/kmeans.png')
 
-silhouette.silhouette_plot(filtered_embedding, kmeans_labels, n_clusters=3)
-silhouette.silhouette_plot(filtered_embedding, filtered_labels, n_clusters=3)
+silhouette.silhouette_plot(filtered_embedding, kmeans_labels, n_clusters=3, save_path='supervised_output/silhouette_plot_kmeans.png')
+silhouette.silhouette_plot(filtered_embedding, filtered_labels, n_clusters=3, save_path='supervised_output/silhouette_plot_dbscan.png')
 
-print(f"K-means Silhouette Score: {silhouette.silhouettescore(filtered_embedding, kmeans_labels)}")
+print(f"\nK-means Silhouette Score: {silhouette.silhouettescore(filtered_embedding, kmeans_labels)}")
 print(f"DBSCAN Silhouette Score: {silhouette.silhouettescore(filtered_embedding, filtered_labels)}")
 
 mask = np.isin(dbscan_labels, filtered_labels)
 filtered_data = Data[mask]
 
-labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=8)
-labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=9)
-labeling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=13)
+labelling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=8, save_path='supervised_output/labelling_ratio_d_t_f.png')
+labelling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=9, save_path='supervised_output/labelling_ratio_d_t_p.png')
+labelling.ordering(filtered_data, labels=filtered_labels, start_time=20, end_time=200, feature_index=13, save_path='supervised_output/labelling_mean_magnitude.png')
 
 df_output = filtered_data.copy()
 df_output['class'] = filtered_labels
-
-os.makedirs('supervised_output', exist_ok=True)
 
 df_output.to_csv('supervised_output/labelled_data.csv', index=False)
