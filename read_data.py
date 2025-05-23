e605e7502f39404ef511683d9aaeac0f733f57b5
leonorf20 <162609318+leonorf20@users.noreply.github.com>
2025-05-23 21:09:29 +0100

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -2,13 +2,18 @@ import os
 import numpy as np
 import h5py
 
-def readData(diretorio, num_files):
+def readData(diretory, start_file, end_file, output_dir):
     base_path = os.path.dirname(__file__)
-    directory = os.path.join(base_path, diretorio)
+    directory = os.path.join(base_path, diretory)
+    output_path = os.path.join(output_dir, f'data_cache_{start_file}_{end_file}.npz')
+
+    if os.path.exists(output_path):
+        print(f"Loading cached data from {output_path} ...")
+        return dict(np.load(output_path, allow_pickle=True))
 
     data_dict = {}
 
-    for i in range(1, num_files + 1):
+    for i in range(start_file, end_file + 1):
         file_path = os.path.join(directory, f'continuous{i}.mat')
 
         if file_path.endswith('.mat'):
@@ -17,10 +22,12 @@ def readData(diretorio, num_files):
                 with h5py.File(file_path, 'r') as f:
                     data = f['data_bin'][:]
                     data = np.array(data, dtype=np.float32)
+                    data = np.delete(data, 0, axis=1) 
                     data_dict[f'continuous{i}'] = data
-                    data_dict[f'continuous{i}'] = np.delete(data_dict[f'continuous{i}'], 0, axis=1)
             except Exception as e:
                 print(f"Error reading file (h5py): {file_path}")
                 print(str(e))
-    
+
+    np.savez(output_path, **data_dict)
+    print(f"\nData cached to {output_path}")
     return data_dict
