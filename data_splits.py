10ab078ab75d074c11598824feb5aa363f9cf839
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-05-24 21:52:28 +0100

    Update data_splits.py

--- data_splits.py
+++ data_splits.py
@@ -2,24 +2,43 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from scipy.io import savemat
 import os
+import numpy as np
 
-def load_and_split_data(filepath='supervised_output/final_labelled_data.csv', label_column='class', train_size=0.7, val_size=0.2, test_size=0.1, random_state=42):
-        
-        df = pd.read_csv(filepath)
-        df = df.sample(frac=1, random_state=random_state).reset_index(drop=True)
+def load_and_split_data(filepath='supervised_output/final_labelled_data.csv', label_column='class', train_size=0.7, random_state=42):
+    df = pd.read_csv(filepath)
+    df = df.sample(frac=1, random_state=random_state).reset_index(drop=False)
 
-        X = df.drop(columns=[label_column])
-        y = df[label_column]
+    X = df.drop(columns=[label_column, 'index'])
+    y = df[label_column]
+    indices = df['index']
 
-        X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=(1.0 - train_size), random_state=random_state)
+    X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split(
+        X, y, indices, test_size=(1.0 - train_size), random_state=random_state)
 
-        val_relative_size = val_size / (val_size + test_size)
+    return X_train, X_test, y_train, y_test, idx_train, idx_test
 
-        X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=(1.0 - val_relative_size), random_state=random_state)
 
-        return X_train, X_val, X_test, y_train, y_val, y_test
-
-def save_Xtest(X_test, directory):
+def save_mat(data, directory, file_name):
     os.makedirs(directory, exist_ok=True)
-    filepath = os.path.join(directory, 'X_test.mat')
-    savemat(filepath, {'X_test': X_test.to_numpy()})
+    
+    if isinstance(data, pd.DataFrame):
+        data_to_save = data.to_numpy()
+    elif isinstance(data, np.ndarray):
+        data_to_save = data
+    else:
+        raise ValueError("Data must be a pandas DataFrame or numpy ndarray")
+    
+    key_name = os.path.splitext(file_name)[0]
+    filepath = os.path.join(directory, file_name)
+    savemat(filepath, {key_name: data_to_save})
+
+def get_original_samples(original_data, feature_indices, fs=1000, window_sec=30):
+    window_size = fs * window_sec
+    original_samples = []
+    
+    for idx in feature_indices:
+        start = idx * window_size
+        end = (idx + 1) * window_size
+        original_samples.append(original_data[start:end])
+    
+    return np.concatenate(original_samples, axis=0)
