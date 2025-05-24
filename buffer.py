4c69f0920a285e60dc61a5f08e8efffadc056e9d
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-05-24 21:50:35 +0100

    Add files via upload

--- buffer.py
+++ buffer.py
@@ -1,25 +1,34 @@
 import numpy as np
-import h5py
+import time
+from scipy.io import loadmat
 
-def read_samples_from_mat(file_path, dataset_name='data_bin', num_channels=6):
-    with h5py.File(file_path, 'r') as f:
-        data = f[dataset_name]
-        n_samples = data.shape[0]
+def read_samples_from_mat(file_path, dataset_name='Data_test'):
+    mat_data = loadmat(file_path)
+    data = mat_data[dataset_name]
+    for sample in data:
+        yield sample
 
-        for i in range(n_samples):
-            sample = data[i][1:]
-            yield sample
 
-def stream_30s_windows(file_paths, fs=1000, window_sec=30, num_channels=5):
+def stream_30s_windows(file_path, fs=1000, window_sec=30):
     window_size = fs * window_sec
     buffer = []
+    buffer_start_time = None
 
-    for file_path in file_paths:
-        print(f"Reading {file_path}")
-        sample_generator = read_samples_from_mat(file_path, num_channels=num_channels + 1)
-        for sample in sample_generator:
-            buffer.append(sample)
-            if len(buffer) == window_size:
-                window = np.array(buffer)
-                yield window
-                buffer = []
+    for sample in read_samples_from_mat(file_path):
+        if buffer_start_time is None:
+            buffer_start_time = time.time()
+
+        buffer.append(sample)
+
+        if len(buffer) == window_size:
+            elapsed_time = time.time() - buffer_start_time
+            remaining_time = max(0, window_sec - elapsed_time)
+            if remaining_time > 0:
+                time.sleep(remaining_time)
+
+            window_array = np.array(buffer)
+            yield {'data': window_array}
+
+            buffer = []
+            buffer_start_time = None
+
--- online.py
+++ online.py
@@ -1,5 +1,33 @@
 import buffer
-files = [r'Continuous\Mouse1\continuous1.mat']
+import threading
+import joblib
+import signal_processing
+import feature_extraction
+import pandas as pd
+import data_processing
 
-for window_30s in buffer.stream_30s_windows(files):
-    print(window_30s)
+stop_flag = False
+
+def listen_for_stop():
+    global stop_flag
+    input("Pressione ENTER para parar...\n")
+    stop_flag = True
+
+threading.Thread(target=listen_for_stop, daemon=True).start()
+
+file_path = r'supervised_output\Data_test.mat'
+svm = joblib.load(r'supervised_output\svm_model.joblib')
+scaler = joblib.load(r'unsupervised_output\scaler.joblib')
+
+for window_30s in buffer.stream_30s_windows(file_path):
+    if stop_flag:
+        print("Execução parada pelo usuário.")
+        break
+    processed_dict = signal_processing.FilterSignals(window_30s, fs=1000)
+    
+    features = feature_extraction.featureVect(processed_dict)
+    df_features = pd.DataFrame(features)
+    Data, _ = data_processing.Standardization(df_features, scaler=scaler)
+    
+    prediction = svm.predict(Data)
+    print(prediction)
