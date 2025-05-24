4d8ecab2b46a7ec9a0c9dd6d9fdb02676aaa458b
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-05-24 15:51:36 +0100

    Update data_splits.py

--- data_splits.py
+++ data_splits.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from scipy.io import savemat
+import os
 
-def load_and_split_data(filepath='unsupervised_output/labelled_data.csv', label_column='class', train_size=0.7, val_size=0.2, test_size=0.1, random_state=42):
+def load_and_split_data(filepath='supervised_output/final_labelled_data.csv', label_column='class', train_size=0.7, val_size=0.2, test_size=0.1, random_state=42):
         
         df = pd.read_csv(filepath)
         df = df.sample(frac=1, random_state=random_state).reset_index(drop=True)
@@ -16,3 +18,8 @@ def load_and_split_data(filepath='unsupervised_output/labelled_data.csv', label_
         X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=(1.0 - val_relative_size), random_state=random_state)
 
         return X_train, X_val, X_test, y_train, y_val, y_test
+
+def save_Xtest(X_test, directory):
+    os.makedirs(directory, exist_ok=True)
+    filepath = os.path.join(directory, 'X_test.mat')
+    savemat(filepath, {'X_test': X_test.to_numpy()})
