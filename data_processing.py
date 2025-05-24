6705272299f5a17fba4e158a886afae78e3a69a1
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-05-24 21:52:17 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,12 @@
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 
-def Standardization(df):
-    scaler = StandardScaler()
-    scaled_array = scaler.fit_transform(df)
+def Standardization(df, scaler=None):
+    if scaler is None:
+        scaler = StandardScaler()
+        scaled_array = scaler.fit_transform(df)
+    else:
+        scaled_array = scaler.transform(df)
+    
     scaled_df = pd.DataFrame(scaled_array, columns=df.columns)
-
-    return scaled_df
+    return scaled_df, scaler
