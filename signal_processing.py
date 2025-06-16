10ea0e689e3bc3ae7f305b955af4278155c2382e
Leandro Ribeiro <154263742+J-u-s-tLee@users.noreply.github.com>
2025-06-16 16:06:43 +0100

    Update signal_processing.py

--- signal_processing.py
+++ signal_processing.py
@@ -10,7 +10,7 @@ def Notch(w0=50, Q=30, fs=1000):
     b, a = signal.iirnotch(w0, Q, fs)
     return b, a
 
-def LowPass(order=4, Wn=5, fs=1000):
+def LowPass(order=4, Wn=50, fs=1000):
     sos = signal.butter(order, Wn, btype='low', fs=fs, output='sos')
     return sos
 
@@ -36,7 +36,7 @@ def FilterSignals(data_dict, fs=1000):
 
     return filtered_dict
 
-def plot_signals(original_dict, filtered_dict, key='continuous1', fs=1000, duration=20):
+def plot_signals(original_dict, filtered_dict, key='continuous1', fs=1000, duration=20, save_path=None):
 
     channel_names = ['EEG Frontal', 'EEG Parietal', 'Acc X', 'Acc Y', 'Acc Z']
     time = np.arange(0, duration, 1/fs)
@@ -48,14 +48,20 @@ def plot_signals(original_dict, filtered_dict, key='continuous1', fs=1000, durat
     fig.suptitle(f"Signals before and after processing ({key})", fontsize=16)
 
     for i in range(5):
-        axes[i, 0].plot(time, original[:, i], color='gray')
+        axes[i, 0].plot(time, original[:, i], color='deepskyblue')
         axes[i, 0].set_ylabel(channel_names[i])
         axes[i, 0].set_title("Original")
 
-        axes[i, 1].plot(time, filtered[:, i], color='green')
-        axes[i, 1].set_title("Filered")
+        axes[i, 1].plot(time, filtered[:, i], color='tomato')
+        axes[i, 1].set_title("Filtered")
 
     axes[-1, 0].set_xlabel("Time (s)")
     axes[-1, 1].set_xlabel("Time (s)")
     plt.tight_layout(rect=[0, 0, 1, 0.97])
-    plt.show()
+    
+    if save_path:
+        plt.savefig(save_path, dpi=300)
+    else:
+        plt.show()
+
+    plt.close()
